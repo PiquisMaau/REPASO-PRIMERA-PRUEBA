7f684df2806e65c64658105c56f54995f500baa3
Mauro Pico <sebaspico@outlook.com>
2026-03-31 22:25:20 -0500

    Repaso con archivos .csv

---
+++ BibliotecaClases/Ejercicio27Logica.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class Ejercicio27Logica
+    {
+        private int[,] matrizDatos;
+
+        public Ejercicio27Logica(int[,] matrizDatos)
+        {
+            this.matrizDatos = matrizDatos;
+        }
+
+        public void SucursalMayorInventario()
+        {
+            int mayorInventario = int.MinValue; 
+            foreach (var item in matrizDatos)
+            {
+                if (item > mayorInventario)
+                {
+                    mayorInventario = item; 
+                }
+            }
+            Console.WriteLine($"El item con mayor valor es: {mayorInventario}");
+        }
+
+        public void ProductoEscaso()
+        {
+            int productoEscaso = int.MaxValue;
+            foreach (var item in matrizDatos)
+            {
+                if (item < productoEscaso)
+                {
+                    productoEscaso = item;
+                }
+            }
+            Console.WriteLine($"El item con menor valor es: {productoEscaso}");
+        }
+
+        public void InventarioTotal()
+        {
+            int sumatoria = 0;
+            foreach (var item in matrizDatos)
+            {
+                sumatoria += item;
+            }
+            Console.WriteLine($"El inventario total es: {sumatoria}");
+        }
+    }
+}
---
+++ Ejercicio27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio27
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //            27.Control de inventario por sucursal
+            //Enunciado:
+            //Una cadena comercial registra en una matriz de 4x8 el número de unidades disponibles
+            //de 8 productos en 4 sucursales.El programa debe determinar qué sucursal tiene mayor
+            //inventario, qué producto está más escaso globalmente y el inventario total. 
+
+            
+            string ruta = "datosEJ27.csv";
+            string[] lineas = File.ReadAllLines(ruta);
+
+            int filas = lineas.Length;
+            int columnas = lineas[0].Split(';').Length;
+            int[,] matrizProductosSucursal = new int[filas, columnas];
+            
+        }
+    }
+}
